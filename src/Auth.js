b9c4a1fcb92c420a806b5acb471edcd74d1a75b8
allwild <100026995+allwild@users.noreply.github.com>
2023-06-30 14:22:09 +0200

    setPersisentce added

--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,10 @@
 import { getAuth } from 'firebase/auth'
 import app from './firebase'
+import firebase from 'firebase/compat/app'
+
+
 
 const auth = getAuth(app);
+auth.setPersistence(firebase.auth.Auth.Persistence.NONE);
 
 export default auth;
